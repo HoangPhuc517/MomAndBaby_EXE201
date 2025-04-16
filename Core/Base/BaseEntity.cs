08993ba9e5592685844b84bdf81bad10c3547c4f
PHAN_LONG_HOANG_PHUC <hgphuc2k3@gmail.com>
2025-04-16 21:23:51 +0700

    Config: First Project

---
+++ Core/Base/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MomAndBaby.Core.Base
+{
+    public abstract class BaseEntity
+    {
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid().ToString();
+            CreatedTime = DateTimeOffset.UtcNow.ToOffset(TimeSpan.FromHours(7));
+            UpdatedTime = CreatedTime;
+        }
+
+        public string Id { get; set; }
+        public string Status { get; set; }
+        public DateTimeOffset CreatedTime { get; set; }
+        public DateTimeOffset UpdatedTime { get; set; }
+    }
+}
