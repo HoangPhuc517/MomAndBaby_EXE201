4fcdbd09f532951d5fffb35e0269f120b1b1e6f8
PHAN_LONG_HOANG_PHUC <hgphuc2k3@gmail.com>
2025-05-21 17:33:10 +0700

    feat: Appointmrnt

---
+++ Core/Store/AppointmentTypeEnum.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MomAndBaby.Core.Store
+{
+    public enum AppointmentTypeEnum
+    {
+        /// <summary>
+        /// Khám trực tiếp tại cơ sở y tế
+        /// </summary>
+        InPerson = 0,
+
+        /// <summary>
+        /// Tư vấn trực tuyến qua video hoặc chat
+        /// </summary>
+        Online = 1,
+
+        /// <summary>
+        /// Tái khám sau điều trị hoặc theo dõi
+        /// </summary>
+        FollowUp = 2,
+
+        /// <summary>
+        /// Khám sức khỏe định kỳ
+        /// </summary>
+        RoutineCheck = 3,
+
+        /// <summary>
+        /// Hẹn để làm xét nghiệm (máu, nước tiểu, v.v.)
+        /// </summary>
+        LabTest = 4,
+
+        /// <summary>
+        /// Hẹn tiêm vắc xin
+        /// </summary>
+        Vaccination = 5,
+
+        /// <summary>
+        /// Tư vấn chung về sức khỏe, dinh dưỡng,...
+        /// </summary>
+        Consultation = 6
+    }
+
+    public enum AppointmentStatusEnum
+    {
+        /// <summary>
+        /// Đã hủy
+        /// </summary>
+        Canceled = 0,
+
+        /// <summary>
+        /// Đã hoàn thành
+        /// </summary>
+        Completed = 1,
+
+        /// <summary>
+        /// Đang chờ
+        /// </summary>
+        Pending = 2
+    }
+
+}
